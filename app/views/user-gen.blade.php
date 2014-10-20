6264e72984774f313e4096fe37676fea75fd6870
Allan Burns <allanlburns@yahoo.com>
2014-10-20 16:19:01 -0400

    User-gen works and returns on view.

--- app/views/user-gen.blade.php
+++ app/views/user-gen.blade.php
@@ -7,7 +7,11 @@ This page will generate random users for you to use! <br><br>
 
 <?php
 
+require_once 'C:\xampp\htdocs\P3\vendor\fzaninotto\Faker\src\autoload.php';
 
+$faker = Faker\Factory::create();
+
+echo $faker->name;
 
 ?>
 
