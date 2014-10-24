12ac66cbc2b5d1c3dc996396c4a877fa592dc15c
Allan Burns <allanlburns@yahoo.com>
2014-10-23 20:38:00 -0400

    Added form to user-gen.

--- app/views/user-gen.blade.php
+++ app/views/user-gen.blade.php
@@ -5,6 +5,11 @@ Random User Generator
 @section('content')
 This page will generate random users for you to use! <br><br>
 
+<p><form name="input" action='lorem' method="post">
+	Number of users (1-20):<input type="number" name="users" min="1" max="20">
+	<input type="submit" value="Submit">
+</form>
+
 <?php
 
 	require_once 'C:\xampp\htdocs\P3\vendor\fzaninotto\Faker\src\autoload.php';
