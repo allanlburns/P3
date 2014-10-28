344d0367c4c39de70a299f0df401abc7feca1619
Allan Burns <allanlburns@yahoo.com>
2014-10-27 20:24:45 -0400

    :

--- app/routes.php
+++ app/routes.php
@@ -31,7 +31,8 @@ Route::post('/lorem', function()
 
 Route::get('/user-gen', function()
 {
-		
+	require_once 'C:\xampp\htdocs\P3\vendor\fzaninotto\Faker\src\autoload.php';
+	
 	return View::make('user-gen');
 });
 
