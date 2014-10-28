0d537b6b06ca88c21e13f58f6930a04ab01bcf11
Allan Burns <allanlburns@yahoo.com>
2014-10-27 20:47:39 -0400

    removed require from routes

--- app/routes.php
+++ app/routes.php
@@ -31,8 +31,7 @@ Route::post('/lorem', function()
 
 Route::get('/user-gen', function()
 {
-	require_once 'C:\xampp\htdocs\P3\vendor\fzaninotto\Faker\src\autoload.php';
-	
+
 	return View::make('user-gen');
 });
 
